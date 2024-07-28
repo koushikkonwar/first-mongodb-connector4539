928a7977da6d31cc20fbf23e6809adcd3f52859e
koushik.konwar <koushik.konwar@sprinklr.com>
2024-07-28 11:48:37 +0530

    config files updated

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup, find_packages
 from typing import List
 
+HYPEN_E_DPT = '-e .'
+
+def get_requirement(file_path:str)->List[str]:
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DPT in requirements:
+            requirements.remove(HYPEN_E_DPT)
+    return requirements
+
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
@@ -24,4 +35,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=get_requirement("./requirements_dev.txt")
 )
